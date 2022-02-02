0c861dfc3de60486455d6bec9e93e59ba681ddb0
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-02 10:24:01 -0400

    page 536

--- Global.asax.cs
+++ Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Views.Infrastructure;
 
 namespace Views
 {
@@ -13,6 +14,14 @@ namespace Views
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // Removing the Other View Engines
+            ViewEngines.Engines.Clear();
+
+            // ViewEngines.Engines.Insert(0, new DebugDataViewEngine()); - the first view engine that MVC will use
+
+            // Registering a Custom View Engine
+            ViewEngines.Engines.Add(new DebugDataViewEngine());
         }
     }
 }
